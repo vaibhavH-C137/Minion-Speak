8a3e8becb4576a082c42152e5caf87b4788ce40e
xoxics <55588029+xoxics@users.noreply.github.com>
2021-05-07 19:44:14 +0530

    intial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputField = document.querySelector("#translate-output");
+
+//function to construct a GET request
+function urlConstructor(text) {
+    return "https://api.funtranslations.com/translate/yoda.json?text=" + text;
+}
+
+//function which performs fetch call
+function clickHandler(text) {
+
+    fetch(urlConstructor(text))
+    .then(function responseHandler(response) {return response.json()})
+    .then (function sendJSON(json){outputField.innerText =  json.contents.translated})
+    .catch((error) => {
+        console.log(error);
+    });
+}
+
+btnTranslate.addEventListener("click", function clickHandler() {
+    
+    clickHandler(txtInput.value);
+});
